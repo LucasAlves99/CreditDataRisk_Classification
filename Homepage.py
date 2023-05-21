466f2f52a2e05160d114d1910ec8052a771d33ac
Lucas Alves <lucasaer10@gmail.com>
2023-05-21 14:50:49 -0300

    Update Homepage.py

--- Homepage.py
+++ Homepage.py
@@ -79,6 +79,18 @@ def main():
     st.header("Projeto de Portfólio Risco de crédito")
     st.write("")
     st.write("Fonte : https://www.kaggle.com/datasets/laotse/credit-risk-dataset")
+
+    st.set_page_config(
+    page_title="Ex-stream-ly Cool App",
+    page_icon="🧊",
+    layout="wide",
+    initial_sidebar_state="expanded",
+    menu_items={
+        'Get Help': 'https://www.extremelycoolapp.com/help',
+        'Report a bug': "https://www.extremelycoolapp.com/bug",
+        'About': "# This is a header. This is an *extremely* cool app!"
+    }
+    )
     
     hide_st_style="""
             <style>
